2a8a4d80d74b3cd3ee46e470db892414420beefb
mcmancini <m.c.mancini@exeter.ac.uk>
2022-06-24 11:33:09 +0100

    Update fcn_run_recreation.m

--- Models/Recreation/fcn_run_recreation.m
+++ Models/Recreation/fcn_run_recreation.m
@@ -392,7 +392,7 @@ function es_recreation = fcn_run_recreation(parameters, hash, rec_baseline_lu, s
 
         % (4.c.1) ORVal LC Parameters
         % ---------------------------
-        [model, params] = fcn_nevo_include_read_params(model, conn); 
+        [model, params] = fcn_nevo_include_read_params(model); 
 
         % (4.c.2) Calculate new expv1 with new landcovers
         % -----------------------------------------------
