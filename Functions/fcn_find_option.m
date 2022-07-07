0c38ae441d784eea3c62e49a8eb6100f9a09093e
mcmancini <m.c.mancini@exeter.ac.uk>
2022-07-07 14:22:51 +0100

    Update fcn_find_option.m

--- Functions/fcn_find_option.m
+++ Functions/fcn_find_option.m
@@ -10,6 +10,7 @@ function option_table = fcn_find_option(options, scenario_land_cover, baseline_l
     baseline_land_cover = baseline_land_cover(:, macro_lc);
     scenario_land_cover = scenario_land_cover(:, macro_lc);
     landuse_chg = scenario_land_cover{:,:} - baseline_land_cover{:,:};
+    landuse_chg = round(landuse_chg, 3);
     landuse_chg = array2table(landuse_chg);
     landuse_chg.Properties.VariableNames = macro_lc;
     
@@ -42,7 +43,14 @@ function option_table = fcn_find_option(options, scenario_land_cover, baseline_l
         option_negative = varfun(is_negative, row);
         which_negative = row.Properties.VariableNames(find(table2array(option_negative)));
         
+        % as farm is the combination of arable and grass, the inclusion of
+        % the farm2xyz options is redundant
+        if any(strcmp(which_negative, 'farm_ha'))
+            which_negative(strcmp(which_negative, 'farm_ha')) = [];
+        end
+        
         option_list = {};
+        counter = 1;
         for j = 1:length(which_negative)
             for k = 1:numel(which_positive)
                 opt_from = which_negative{j};
@@ -51,19 +59,20 @@ function option_table = fcn_find_option(options, scenario_land_cover, baseline_l
                 opt_to = which_positive{k};
                 opt_to = split(opt_to, '_');
                 opt_to = opt_to{1};
-                option_list{j} = strcat(opt_from, '2', opt_to);
+                option_list{counter} = strcat(opt_from, '2', opt_to);
+                counter = counter + 1;
             end
         end
         
         if(isempty(option_list))
             continue
         else
-            % as farm is the combination of arable and grass, the inclusion of
-            % the farm2xyz options is redundant
-            if any(contains(option_list, 'farm'))
-                idx = contains(option_list, 'farm');
-                option_list(idx) = [];                    
-            end
+%             % as farm is the combination of arable and grass, the inclusion of
+%             % the farm2xyz options is redundant
+%             if any(contains(option_list, 'farm'))
+%                 idx = contains(option_list, 'farm');
+%                 option_list(idx) = [];                    
+%             end
             option_table{i, option_list} = ones(1, length(option_list));
         end
     end
