a10bfc02219b9d6524b007f8fa276c1e9c7490ef
mcmancini <m.c.mancini@exeter.ac.uk>
2023-02-24 09:41:01 +0000

    Update fcn_run_landuse_change.m

--- Models/Water Transfer/fcn_run_landuse_change.m
+++ Models/Water Transfer/fcn_run_landuse_change.m
@@ -38,7 +38,7 @@ function fcn_run_landuse_change(uk_baseline, options, parameters, hash)
     % Add arable and tgrass hectares to semi-natural grassland hectares
     % Subtract arable and tgrass hectares from farm hectares
     % Reduce all crops and farm grassland (except permanent grassland and rough grazing) to zero
-    if do_arable2sng
+    if exist('do_arable2sng', 'var')
         lcs_arable2sng = lcs_baseline;
         hectares = lcs_arable2sng.arable_ha_20 + lcs_arable2sng.tgrass_ha_20;
         lcs_arable2sng.hectares = hectares;
@@ -117,7 +117,7 @@ function fcn_run_landuse_change(uk_baseline, options, parameters, hash)
     
     %% (2) Scenario land uses: arable2wood
     %  ===================================
-    if do_arable2wood
+    if exist('do_arable2wood', 'var')
         % Land cover change
         % -----------------
         % Add arable and tgrass hectares to woodland hectares
@@ -204,7 +204,7 @@ function fcn_run_landuse_change(uk_baseline, options, parameters, hash)
     % Add arable and tgrass hectares to urban hectares
     % Subtract arable and tgrass hectares from farm hectares
     % Reduce all crops and farm grassland (except permanent grassland and rough grazing) to zero
-    if do_arable2urban
+    if exist('do_arable2urban', 'var')
         lcs_arable2urban = lcs_baseline;
         hectares = lcs_arable2urban.arable_ha_20 + lcs_arable2urban.tgrass_ha_20;
         lcs_arable2urban.hectares = hectares;
@@ -286,7 +286,7 @@ function fcn_run_landuse_change(uk_baseline, options, parameters, hash)
     % Add arable and tgrass hectares to a mix of 50% woodland and 50% sng
     % Subtract arable and tgrass hectares from farm hectares
     % Reduce all crops and farm grassland (except permanent grassland and rough grazing) to zero
-    if do_arable2mixed
+    if exist('do_arable2mixed', 'var')
         lcs_arable2mixed = lcs_baseline;
         hectares = lcs_arable2mixed.arable_ha_20 + lcs_arable2mixed.tgrass_ha_20;
         lcs_arable2mixed.hectares = hectares;
@@ -365,7 +365,7 @@ function fcn_run_landuse_change(uk_baseline, options, parameters, hash)
 
     %% (5) Scenario land uses: grass2sng
     %  =================================
-    if do_grass2sng
+    if exist('do_grass2sng', 'var')
         % Land cover change
         % -----------------
         % Add pgrass and rgraz hectares to semi-natural grassland hectares
@@ -469,7 +469,7 @@ function fcn_run_landuse_change(uk_baseline, options, parameters, hash)
 
     %% (6) Scenario land uses: grass2wood
     %  ==================================
-    if do_grass2wood
+    if exist('do_grass2wood', 'var')
         % Land cover change
         % -----------------
         % Add pgrass and rgraz hectares to woodland hectares
@@ -574,7 +574,7 @@ function fcn_run_landuse_change(uk_baseline, options, parameters, hash)
     
     %% (7) Scenario land uses: grass2urban
     %  ===================================
-    if do_grass2urban
+    if exist('do_grass2urban', 'var')
         % Land cover change
         % -----------------
         % Add pgrass and rgraz hectares to urban hectares
@@ -679,7 +679,7 @@ function fcn_run_landuse_change(uk_baseline, options, parameters, hash)
     
     %% (8) Scenario land uses: grass2mixed
     %  ===================================
-    if do_grass2mixed
+    if exist('do_grass2mixed', 'var')
         % Land cover change
         % -----------------
         % Add pgrass and rgraz hectares to a mix of 50% wood and 50% sng
@@ -785,7 +785,7 @@ function fcn_run_landuse_change(uk_baseline, options, parameters, hash)
     
     %% (9) Scenario land uses: sng2urban
     %  =================================
-    if do_sng2urban
+    if exist('do_sng2urban', 'var')
         % Land cover change
         % -----------------
         % Add sngrass hectares to urban hectares
@@ -893,7 +893,7 @@ function fcn_run_landuse_change(uk_baseline, options, parameters, hash)
     
     %% (10) Scenario land uses: wood2urban
     %  ===================================
-    if do_wood2urban
+    if exist('do_wood2urban', 'var')
         % Land cover change
         % -----------------
         % Add woodland hectares to urban hectares
@@ -997,5 +997,194 @@ function fcn_run_landuse_change(uk_baseline, options, parameters, hash)
         % -------------------------------
         savename = strcat(save_folder, '\lcs_wood2urban.mat');
         save(savename, 'lcs_wood2urban', '-mat', '-v6');
+    end
+    
+    %% (11) Scenario land uses: arable2thirds
+    %  ======================================
+    
+    % Add arable and tgrass hectares to a mix of 330% woodland and 67% sng
+    % Subtract arable and tgrass hectares from farm hectares
+    % Reduce all crops and farm grassland (except permanent grassland and rough grazing) to zero
+    if exist('do_arable2thirds', 'var')
+        lcs_arable2thirds = lcs_baseline;
+        hectares = lcs_arable2thirds.arable_ha_20 + lcs_arable2thirds.tgrass_ha_20;
+        lcs_arable2thirds.hectares = hectares;
+
+        lcs_arable2thirds.wood_ha = lcs_arable2thirds.wood_ha + 0.33 .* hectares;
+        lcs_arable2thirds.sngrass_ha = lcs_arable2thirds.sngrass_ha + 0.67 .* hectares;
+        lcs_arable2thirds.farm_ha = lcs_arable2thirds.farm_ha - hectares;
+
+        % Set appropriate land covers to zero
+        cols_to_zero = {'arable_ha_20', 'arable_ha_30', 'arable_ha_40', 'arable_ha_50', ...
+                        'wheat_ha_20', 'wheat_ha_30', 'wheat_ha_40', 'wheat_ha_50', ...
+                        'osr_ha_20', 'osr_ha_30', 'osr_ha_40', 'osr_ha_50', ...
+                        'wbar_ha_20', 'wbar_ha_30', 'wbar_ha_40', 'wbar_ha_50', ...
+                        'sbar_ha_20', 'sbar_ha_30', 'sbar_ha_40', 'sbar_ha_50', ...
+                        'pot_ha_20', 'pot_ha_30', 'pot_ha_40', 'pot_ha_50', ...
+                        'sb_ha_20', 'sb_ha_30', 'sb_ha_40', 'sb_ha_50', ...
+                        'other_ha_20', 'other_ha_30', 'other_ha_40', 'other_ha_50', ...
+                        'tgrass_ha_20', 'tgrass_ha_30', 'tgrass_ha_40', 'tgrass_ha_50'};
+        lcs_arable2thirds(:, cols_to_zero) = array2table(zeros(size(lcs_arable2thirds, 1), length(cols_to_zero)));
+
+        % Set remaining farmland grass types to 2020s amount
+        lcs_arable2thirds.rgraz_ha_30 = lcs_arable2thirds.rgraz_ha_20;
+        lcs_arable2thirds.rgraz_ha_40 = lcs_arable2thirds.rgraz_ha_20;
+        lcs_arable2thirds.rgraz_ha_50 = lcs_arable2thirds.rgraz_ha_20;
+
+        lcs_arable2thirds.pgrass_ha_30 = lcs_arable2thirds.pgrass_ha_20;
+        lcs_arable2thirds.pgrass_ha_40 = lcs_arable2thirds.pgrass_ha_20;
+        lcs_arable2thirds.pgrass_ha_50 = lcs_arable2thirds.pgrass_ha_20;
+
+        % Reset total farming grassland
+        lcs_arable2thirds.grass_ha_20 = lcs_arable2thirds.rgraz_ha_20 + lcs_arable2thirds.pgrass_ha_20;
+        lcs_arable2thirds.grass_ha_30 = lcs_arable2thirds.rgraz_ha_30 + lcs_arable2thirds.pgrass_ha_30;
+        lcs_arable2thirds.grass_ha_40 = lcs_arable2thirds.rgraz_ha_40 + lcs_arable2thirds.pgrass_ha_40;
+        lcs_arable2thirds.grass_ha_50 = lcs_arable2thirds.rgraz_ha_50 + lcs_arable2thirds.pgrass_ha_50;
+        
+        % check that land uses sum to 400 hectares and stop it not
+        [lcm, top_level, crops] = fcn_landuse_check(lcs_arable2sng);
+        
+        if top_level ~= 1
+            warning('The 5 lcm land uses for the ''arable2mixed_6633'' option do not sum to 400 ha!')
+        end
+        
+        if any(top_level == 0)
+            decade_list = {'20', '30', '40', '50'};
+            warning_vars = {};
+            ind = find(top_level == 0);
+            for i = 1:length(ind)
+                decade = decade_list(ind(i));
+                warning_vars = [warning_vars, decade];
+            end
+            msg = strcat('arable and grassland hectares for the ''arable2mixed_6633'' option do not sum to farmland hectares for the following decades: \n', ...
+                         repmat('-  %s\n', 1, length(warning_vars))); 
+            warning_msg = sprintf(msg, warning_vars{:});
+            warning(warning_msg)
+        end
+            
+        if any(crops == 0)
+            decade_list = {'20', '30', '40', '50'};
+            warning_vars = {};
+            ind = find(crops == 0);
+            for i = 1:length(ind)
+                decade = decade_list(ind(i));
+                warning_vars = [warning_vars, decade];
+            end
+            msg = strcat('Crops and grassland for the ''arable2mixed_6633'' option do not sum to farmland hectares for the following decades: \n', ...
+                         repmat('-  %s\n', 1, length(warning_vars))); 
+            warning_msg = sprintf(msg, warning_vars{:});
+            warning(warning_msg)
+        end
+
+        % Save to lcs_arable2mixed.mat file
+        % -------------------------------
+        savename = strcat(save_folder, '\lcs_arable2thirds.mat');
+        save(savename, 'lcs_arable2thirds', '-mat', '-v6');
+    end
+    
+%% (8) Scenario land uses: grass2mixed
+    %  ===================================
+    if exist('do_grass2thirds', 'var')
+        % Land cover change
+        % -----------------
+        % Add pgrass and rgraz hectares to a mix of 50% wood and 50% sng
+        % Subtract pgrass and rgraz hectares from farm hectares
+        % Reduce pgrass and rgraz to zero
+        lcs_grass2thirds = lcs_baseline;
+        hectares = lcs_grass2thirds.pgrass_ha_20 + lcs_grass2thirds.rgraz_ha_20;
+        lcs_grass2thirds.hectares = hectares;
+
+        lcs_grass2thirds.wood_ha = lcs_grass2thirds.wood_ha + 0.33 .* hectares;
+        lcs_grass2thirds.sngrass_ha = lcs_grass2thirds.sngrass_ha + 0.67 .* hectares;
+        lcs_grass2thirds.farm_ha = lcs_grass2thirds.farm_ha - hectares;
+
+        % Set appropriate land covers to zero
+        cols_to_zero = {'pgrass_ha_20', 'pgrass_ha_30', 'pgrass_ha_40', 'pgrass_ha_50', ...
+                        'rgraz_ha_20', 'rgraz_ha_30', 'rgraz_ha_40', 'rgraz_ha_50'};
+        lcs_grass2thirds(:, cols_to_zero) = array2table(zeros(size(lcs_grass2thirds, 1), length(cols_to_zero)));
+
+        % Set remaining farmland grass types to 2020s amount
+        lcs_grass2thirds.arable_ha_30 = lcs_grass2thirds.arable_ha_20;
+        lcs_grass2thirds.arable_ha_40 = lcs_grass2thirds.arable_ha_20;
+        lcs_grass2thirds.arable_ha_50 = lcs_grass2thirds.arable_ha_20;
+
+        lcs_grass2thirds.wheat_ha_30 = lcs_grass2thirds.wheat_ha_20;
+        lcs_grass2thirds.wheat_ha_40 = lcs_grass2thirds.wheat_ha_20;
+        lcs_grass2thirds.wheat_ha_50 = lcs_grass2thirds.wheat_ha_20;
+
+        lcs_grass2thirds.osr_ha_30 = lcs_grass2thirds.osr_ha_20;
+        lcs_grass2thirds.osr_ha_40 = lcs_grass2thirds.osr_ha_20;
+        lcs_grass2thirds.osr_ha_50 = lcs_grass2thirds.osr_ha_20;
+
+        lcs_grass2thirds.wbar_ha_30 = lcs_grass2thirds.wbar_ha_20;
+        lcs_grass2thirds.wbar_ha_40 = lcs_grass2thirds.wbar_ha_20;
+        lcs_grass2thirds.wbar_ha_50 = lcs_grass2thirds.wbar_ha_20;
+
+        lcs_grass2thirds.sbar_ha_30 = lcs_grass2thirds.sbar_ha_20;
+        lcs_grass2thirds.sbar_ha_40 = lcs_grass2thirds.sbar_ha_20;
+        lcs_grass2thirds.sbar_ha_50 = lcs_grass2thirds.sbar_ha_20;
+
+        lcs_grass2thirds.pot_ha_30 = lcs_grass2thirds.pot_ha_20;
+        lcs_grass2thirds.pot_ha_40 = lcs_grass2thirds.pot_ha_20;
+        lcs_grass2thirds.pot_ha_50 = lcs_grass2thirds.pot_ha_20;
+
+        lcs_grass2thirds.sb_ha_30 = lcs_grass2thirds.sb_ha_20;
+        lcs_grass2thirds.sb_ha_40 = lcs_grass2thirds.sb_ha_20;
+        lcs_grass2thirds.sb_ha_50 = lcs_grass2thirds.sb_ha_20;
+
+        lcs_grass2thirds.other_ha_30 = lcs_grass2thirds.other_ha_20;
+        lcs_grass2thirds.other_ha_40 = lcs_grass2thirds.other_ha_20;
+        lcs_grass2thirds.other_ha_50 = lcs_grass2thirds.other_ha_20;
+
+        lcs_grass2thirds.tgrass_ha_30 = lcs_grass2thirds.tgrass_ha_20;
+        lcs_grass2thirds.tgrass_ha_40 = lcs_grass2thirds.tgrass_ha_20;
+        lcs_grass2thirds.tgrass_ha_50 = lcs_grass2thirds.tgrass_ha_20;
+
+        % Reset total farming grassland
+        lcs_grass2thirds.grass_ha_20 = lcs_grass2thirds.tgrass_ha_20;
+        lcs_grass2thirds.grass_ha_30 = lcs_grass2thirds.tgrass_ha_30;
+        lcs_grass2thirds.grass_ha_40 = lcs_grass2thirds.tgrass_ha_40;
+        lcs_grass2thirds.grass_ha_50 = lcs_grass2thirds.tgrass_ha_50;
+        
+        % check that land uses sum to 400 hectares and stop it not
+        [lcm, top_level, crops] = fcn_landuse_check(lcs_arable2sng);
+        
+        if top_level ~= 1
+            warning('The 5 lcm land uses for the ''grass2mixed'' option do not sum to 400 ha!')
+        end
+        
+        if any(top_level == 0)
+            decade_list = {'20', '30', '40', '50'};
+            warning_vars = {};
+            ind = find(top_level == 0);
+            for i = 1:length(ind)
+                decade = decade_list(ind(i));
+                warning_vars = [warning_vars, decade];
+            end
+            msg = strcat('arable and grassland hectares for the ''grass2mixed'' option do not sum to farmland hectares for the following decades: \n', ...
+                         repmat('-  %s\n', 1, length(warning_vars))); 
+            warning_msg = sprintf(msg, warning_vars{:});
+            warning(warning_msg)
+        end
+            
+        if any(crops == 0)
+            decade_list = {'20', '30', '40', '50'};
+            warning_vars = {};
+            ind = find(crops == 0);
+            for i = 1:length(ind)
+                decade = decade_list(ind(i));
+                warning_vars = [warning_vars, decade];
+            end
+            msg = strcat('Crops and grassland for the ''grass2mixed'' option do not sum to farmland hectares for the following decades: \n', ...
+                         repmat('-  %s\n', 1, length(warning_vars))); 
+            warning_msg = sprintf(msg, warning_vars{:});
+            warning(warning_msg)
+        end
+
+
+        % Save to lcs_grass2thirds.mat file
+        % -------------------------------
+        savename = strcat(save_folder, '\lcs_grass2thirds.mat');
+        save(savename, 'lcs_grass2thirds', '-mat', '-v6');
     end
 end
